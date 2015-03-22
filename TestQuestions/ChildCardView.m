2a56bb78a7d80f5ed8610049a2c41eab8c68f441
Dennis <learan.85@gmail.com>
2015-03-22 18:40:18 +0200

    child and parent views

---
+++ TestQuestions/ChildCardView.m
@@ -0,0 +1,21 @@
+//
+//  ChildCardView.m
+//  TestQuestions
+//
+//  Created by Hackathon on 22/03/15.
+//  Copyright (c) 2015 ArtfulBits. All rights reserved.
+//
+
+#import "ChildCardView.h"
+
+@implementation ChildCardView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
